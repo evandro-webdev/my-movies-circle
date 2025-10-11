0ec22527a439d02452765b64c85fb74164ce0c3f
evandro-webdev <evandromateus06@gmail.com>
2025-10-11 08:26:00 -0300

    wip

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBpSXsr8ZC_BmCnaySCp42NexSrTFTzHtg",
+  authDomain: "my-movie-circle.firebaseapp.com",
+  projectId: "my-movie-circle",
+  storageBucket: "my-movie-circle.firebasestorage.app",
+  messagingSenderId: "509365004119",
+  appId: "1:509365004119:web:cac637bcf57aaaef1c4012"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const snapshotMovies = await getDocs(collection(db, 'movies'));
+const watchedMovies = snapshotMovies.docs.map(doc => doc.data());
+const watchedMoviesIds = watchedMovies.map(movie => movie.id);
+
 const moviesListEl = document.querySelector('#movies-list');
 const searchForm = document.querySelector('#search-form');
 const searchInput = document.querySelector('#search-input');
 
-const POPULAR_URL = 'https://api.themoviedb.org/3/movie/popular?language=pt-BR&page=1'
 const BASE_URL = 'https://api.themoviedb.org/3/search/movie?language=pt-BR';
+const SINGLE_MOVIE_URL = 'https://api.themoviedb.org/3/movie/';
+const POPULAR_URL = 'https://api.themoviedb.org/3/movie/popular?language=pt-BR&page=1'
 const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
 
 const options = {
@@ -13,6 +33,24 @@ const options = {
     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YjkwZTEzYWY1MDgyNWNlMDI2M2ZjOWQxOTdjOWU4YSIsIm5iZiI6MTYyOTkzNzQzOS45MTM5OTk4LCJzdWIiOiI2MTI2ZGYxZmFhZjg5NzAwNDQ3ZjlhMzUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.WdS9xgyX4eaResQBc18BQSZ3eIzZR6shPU6mZOh0GM8'
   }
 };
+
+async function getSingleMovie(movieId){
+  const url = SINGLE_MOVIE_URL + movieId + '?language=pt-BR';
+
+  return await fetch(url, options)
+    .then(res => res.json())
+    .then(res => res)
+    .catch(err => console.error(err));
+}
+
+const watchedMoviesInfo = await Promise.all(
+  watchedMovies.map(async movie => {
+    const dadosTMDB = await getSingleMovie(movie.id);
+    return { ...movie, poster_path: dadosTMDB.poster_path, genres: dadosTMDB.genres }
+  })
+)
+
+document.addEventListener('DOMContentLoaded', showMovies(watchedMoviesInfo));
 
 searchForm.addEventListener('submit', e => {
   e.preventDefault();
@@ -57,43 +95,60 @@ function showMovies(moviesList){
         </div>
       </div>
     `
-    movieCard.addEventListener('click', () => openMovieModal(movie));
+
+    movieCard.addEventListener('click', () => getMovie(movie.id));
     moviesListEl.append(movieCard);
   });
 }
 
+async function getMovie(movieId){
+  const url = SINGLE_MOVIE_URL + movieId + '?language=pt-BR';
+
+  return await fetch(url, options)
+    .then(res => res.json())
+    .then(res => openMovieModal(res))
+    .catch(err => console.error(err));
+}
+
 function openMovieModal(movie){
   let movieModal = document.createElement("div");
+  movieModal.className = 'overflow-auto fixed inset-0 w-full h-full p-4 bg-black/40 flex justify-center items-center z-50';
+  movieModal.id = 'movie-modal';
+
+  let modalContent = document.createElement("div");
+  modalContent.className = 'w-md p-4 space-y-2 rounded-lg bg-gray-200';
+  modalContent.id = 'modal-content';
 
   let posterPath = movie.poster_path ? BASE_IMAGE_URL + movie.poster_path : '';
 
-  let saveMovieBtn = document.createElement('button');
-  saveMovieBtn.className = 'w-full cursor-pointer mt-6 py-2 px-3 rounded-lg text-white bg-blue-600 hover:bg-blue-700'
-  saveMovieBtn.textContent = 'Marcar como assistido'
-  saveMovieBtn.addEventListener('click', () => saveMovie(movie.id, movie.title, movie.vote_average))
-
-  movieModal.className = 'fixed inset-0 w-full h-full p-4 bg-black/40 flex justify-center items-center z-50';
-
-  movieModal.innerHTML = `
-    <div id="movie-content" class="w-md p-4 space-y-2 rounded-lg bg-gray-200">
-      <div class="w-full h-[90%] rounded-lg overflow-hidden">
-        <img 
-          src="${posterPath}" 
-          alt="${movie.title}" 
-          class="w-full h-full object-cover transition-transform duration-500 ease-out hover:scale-110"
-          onerror="this.src='../img/placeholder.jpg';"
-        >
-      </div>
-      <div>
-        <h2 class="text-lg font-bold text-slate-900">${movie.title}</h2>
-        <p class="text-sm text-slate-700">${movie.overview}</p>
-      </div>
-      <span class="text-sm text-slate-600">Nota: ${movie.vote_average}</span>
+  modalContent.innerHTML = `
+    <div class="w-full h-[90%] rounded-lg overflow-hidden">
+      <img 
+        src="${posterPath}" 
+        alt="${movie.title}" 
+        class="w-full h-full object-cover transition-transform duration-500 ease-out hover:scale-110"
+        onerror="this.src='../img/placeholder.jpg';"
+      >
     </div>
+    <div>
+      <h2 class="text-lg font-bold text-slate-900">${movie.title}</h2>
+      <p class="text-sm text-slate-700">${movie.overview}</p>
+    </div>
+    <div class="my-4 flex flex-wrap gap-2">
+      ${movie.genres.map(g => `<span class="text-xs bg-gray-300 px-2 py-1 rounded">${g.name}</span>`).join('')}
+    </div>
+    <span class="text-sm text-slate-600">Nota: ${movie.vote_average}</span>
   `;
 
-  const movieContent = movieModal.querySelector('#movie-content');
-  movieContent.appendChild(saveMovieBtn);
+  if(!isAlreadyWatched(movie.id)){
+    let saveMovieBtn = document.createElement('button');
+    saveMovieBtn.className = 'w-full cursor-pointer mt-6 py-2 px-3 rounded-lg text-white bg-blue-600 hover:bg-blue-700'
+    saveMovieBtn.textContent = 'Marcar como assistido'
+    saveMovieBtn.addEventListener('click', () => openRatingModal(movie))
+    modalContent.appendChild(saveMovieBtn);
+  }
+
+  movieModal.appendChild(modalContent);
 
   movieModal.addEventListener('click', (e) => {
     if(e.target === movieModal){
@@ -104,26 +159,105 @@ function openMovieModal(movie){
   document.body.appendChild(movieModal);
 }
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
+function openRatingModal(movie){
+  const reviewers = ["evandro", "tauane", "kauane"];
+  const ratings = {};
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBpSXsr8ZC_BmCnaySCp42NexSrTFTzHtg",
-  authDomain: "my-movie-circle.firebaseapp.com",
-  projectId: "my-movie-circle",
-  storageBucket: "my-movie-circle.firebasestorage.app",
-  messagingSenderId: "509365004119",
-  appId: "1:509365004119:web:cac637bcf57aaaef1c4012"
-};
+  let currentReviewerIndex = 0;
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+  let posterPath = movie.poster_path ? BASE_IMAGE_URL + movie.poster_path : '';
+  const movieModal = document.querySelector('#movie-modal');
+  const modalContent = document.querySelector('#modal-content');
 
-async function saveMovie(movieId, movieTitle, movieVote) {
-  await addDoc(collection(db, "filmes"), {
-    id: movieId,
-    titulo: movieTitle,
-    nota: movieVote,
-    data: new Date(),
+  function renderStep(){
+    const reviewer = reviewers[currentReviewerIndex];
+
+    modalContent.innerHTML = `
+      <div id="movie-content" class="p-4 space-y-2 rounded-lg bg-gray-200">
+        <div class="w-32 mb-4 mx-auto aspect-[2/3] rounded-lg overflow-hidden">
+          <img 
+            src="${posterPath}" 
+            alt="${movie.title}" 
+            class="w-full h-full object-cover transition-transform duration-500 ease-out hover:scale-110"
+            onerror="this.src='../img/placeholder.jpg';"
+          >
+        </div>
+        <div class="mb-4 text-center">
+          <h2 class="text-lg font-bold text-slate-900">${movie.title}</h2>
+          <p class="text-sm text-slate-700">${reviewer.charAt(0).toUpperCase() + reviewer.slice(1)}, que nota você da para esse filme?</p>
+        </div>
+        <input 
+          type="number" 
+          id="rating-input" 
+          min="0" 
+          max="10" 
+          step="0.5" 
+          class="w-full border border-gray-300 rounded-lg p-2 text-center focus:outline-none focus:ring-2 focus:ring-blue-500" 
+          placeholder="Digite uma nota de 0 a 10"
+        >
+        <button 
+          id="next-btn" 
+          class="ml-auto px-4 py-2 rounded-lg text-white bg-blue-600 hover:bg-blue-700 transition-colors block"
+        >
+          Próximo →
+        </button>
+      </div>
+    `;
+  }
+
+  renderStep()
+
+  modalContent.addEventListener('click', (e) => {
+    if(e.target.id === 'next-btn'){
+      const input = document.querySelector("#rating-input");
+      const value = parseFloat(input.value);
+
+      if(isNaN(value) || value < 0 || value > 10){
+        alert("Por favor né, digite uma nota de 0 a 10");
+        return;
+      }
+
+      const reviewer = reviewers[currentReviewerIndex];
+      ratings[reviewer] = value;
+
+      currentReviewerIndex++;
+
+      if(currentReviewerIndex < reviewers.length){
+        renderStep();
+      } else {
+        movieModal.remove()
+        saveMovie(movie, ratings);
+      }
+    }
+  })
+}
+
+function isAlreadyWatched(movieId){
+  return watchedMoviesIds.includes(movieId);
+}
+
+async function saveMovie(movie, ratings) {
+  if(isAlreadyWatched(movie.id)){
+    return;
+  }
+
+  const ratingValues = Object.values(ratings);
+  const average_rating = Number(
+    (ratingValues.reduce((a, b) => a + b, 0) / ratingValues.length).toFixed(1)
+  )
+
+  await addDoc(collection(db, "movies"), {
+    id: movie.id,
+    title: movie.title,
+    original_title: movie.original_title,
+    watched_at: new Date().toISOString(),
+    ratings,
+    average_rating,
+    imdb_rating: movie.vote_average,
+    review: 'Filmaço hein!',
+    created_at: new Date(),
+    updated_at: new Date()
   });
+
+  alert(`O filme "${movie.title}" foi salvo com a nota: ${average_rating}!`);
 }
